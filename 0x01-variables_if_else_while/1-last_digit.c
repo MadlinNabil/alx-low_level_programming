bc40b4e9c73fa7c844a57b859a90c291c3b61c64
Madlin Nabil <madlinnabilfekry@gmail.com>
2023-08-24 00:47:48 -0700

    new command

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit of the random excute and print.
- * number stored in the variable n 
- * Return: Always 0 (Success case)
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
